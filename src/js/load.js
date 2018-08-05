b7114bb862a5abf1801418ebb46ddc2d70e12483
Carl <CarlLeninPacheco@gmail.com>
2018-08-05 15:56:45 -0700

    Testing audio

--- src/js/load.js
+++ src/js/load.js
@@ -1,7 +1,11 @@
 var load_state = {  
     preload: function() { 
         this.game.stage.backgroundColor = '#F9F7F3';
+
+        //Main Menu Files
         game.load.image('MainMenuBackground', 'assets/img/Placeholder_Background.png');
+        game.load.audio('MainMenuMusic', 'assets/audio/Placeholder_MainMenuMusic.ogg');
+
         game.load.image('dialogue-bg', 'assets/img/dialogue-bg.png');
         game.load.image('child', 'assets/img/block-player.png');
         game.load.json('dialogue', 'assets/json/dialogue.json');
--- src/js/menu.js
+++ src/js/menu.js
@@ -2,17 +2,21 @@ var menu_state = {
     create: function() 
     {
         var background = game.add.sprite(0, 0, 'MainMenuBackground');
-        // Call the 'start' function when pressing the spacebar
-        var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        spaceKey.onDown.add(this.start, this); 
-
-        
         var style = { font: "30px Helvetica", fill: "#ffffff" };
+        
+        music = game.add.audio('MainMenuMusic');
+        music.play();
+
         //game.add.text(200, 210, "Cairo", style);
 
         //Create start help text
         var startText = game.add.text(0, 0, "Press Spacebar to Start", style);
         startText.alignIn(background, Phaser.BOTTOM_CENTER, 0, -100);
+
+        // Call the 'start' function when pressing the spacebar
+        var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        spaceKey.onDown.add(this.start, this); 
+
     },
 
     // Start the actual game
